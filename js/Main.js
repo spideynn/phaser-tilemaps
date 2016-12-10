86d2433195a1d0237575e56ee5681fb0a06645a5
Spideynn <spideynn@gmail.com>
2016-12-10 12:36:58 -0500

    Add files

---
+++ js/Main.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phasergame', {preload: preload, create: create, update: update, render: render });
+	var map;
+	var layer;
+
+	function preload() {
+
+	    game.load.tilemap('map', '../assets/tilemap/maze.json', null, Phaser.Tilemap.TILED_JSON);
+	    game.load.image('tiles', '../assets/tilemap/tiles.png');
+
+	}
+
+	function create() {
+	    map = game.add.tilemap('map');
+	    map.addTilesetImage('tiles', 'tiles');
+	    layer = map.createLayer('Walls');
+	    layer.resizeWorld();
+	}
+
+	function update() {}
+
+	function render() {}
+};
