c1e979450b43b89a2971402b4466cfb5b172ad4a
Spideynn <spideynn@gmail.com>
2016-12-12 10:17:07 -0500

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -12,7 +12,7 @@ window.onload = function() {
 
 	function create() {
 	    map = game.add.tilemap('map');
-	    map.addTilesetImage('tilemap.png', 'tiles');
+	    map.addTilesetImage('tilemap', 'tiles');
 	    layer = map.createLayer('Walls');
 	    layer.resizeWorld();
 	}
