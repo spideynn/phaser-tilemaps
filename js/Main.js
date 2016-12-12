129e0a25701b21fb0947bf508e9ce9c0b1b7a74d
Spideynn <spideynn@gmail.com>
2016-12-12 10:18:55 -0500

    Revert previous commit

--- js/Main.js
+++ js/Main.js
@@ -12,7 +12,7 @@ window.onload = function() {
 
 	function create() {
 	    map = game.add.tilemap('map');
-	    map.addTilesetImage('tilemap', 'tiles');
+	    map.addTilesetImage('tilemap.png', 'tiles');
 	    layer = map.createLayer('Walls');
 	    layer.resizeWorld();
 	}
